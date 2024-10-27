6d84d0c72326fc37eabfb84d0e3815040b00d082
sophiarupina13 <sophiarupina13@gmail.com>
2024-10-28 01:08:36 +0300

    adaptive p3

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,59 +1,45 @@
 "use client";
 import styles from "../app/page.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 const pictures = [
-  {
-    id: 1,
-    image: "/img/gallery1.png",
-  },
-  {
-    id: 2,
-    image: "/img/gallery2.png",
-  },
-  {
-    id: 3,
-    image: "/img/gallery3.png",
-  },
-  {
-    id: 4,
-    image: "/img/gallery4.png",
-  },
-  {
-    id: 5,
-    image: "/img/gallery5.png",
-  },
-  {
-    id: 6,
-    image: "/img/gallery6.png",
-  },
-  {
-    id: 7,
-    image: "/img/gallery7.png",
-  },
-  {
-    id: 8,
-    image: "/img/gallery8.png",
-  },
-  {
-    id: 9,
-    image: "/img/gallery9.png",
-  },
+  { id: 1, image: "/img/gallery1.png" },
+  { id: 2, image: "/img/gallery2.png" },
+  { id: 3, image: "/img/gallery3.png" },
+  { id: 4, image: "/img/gallery4.png" },
+  { id: 5, image: "/img/gallery5.png" },
+  { id: 6, image: "/img/gallery6.png" },
+  { id: 7, image: "/img/gallery7.png" },
+  { id: 8, image: "/img/gallery8.png" },
+  { id: 9, image: "/img/gallery9.png" },
 ];
 
 const Gallery = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 1000);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleScroll = (direction) => {
+    const step = isMobileView ? 1 : 3;
     if (direction === "left" && currentIndex > 0) {
-      setCurrentIndex(currentIndex - 3);
-    } else if (direction === "right" && currentIndex < pictures.length - 3) {
-      setCurrentIndex(currentIndex + 3);
+      setCurrentIndex(currentIndex - step);
+    } else if (direction === "right" && currentIndex < pictures.length - step) {
+      setCurrentIndex(currentIndex + step);
     }
   };
 
-  const visiblePictures = pictures.slice(currentIndex, currentIndex + 3);
+  const visiblePictures = pictures.slice(currentIndex, currentIndex + (isMobileView ? 1 : 3));
 
   return (
     <section className={styles.sectionGallery}>
@@ -61,21 +47,22 @@ const Gallery = () => {
         <h2>ГАЛЕРЕЯ</h2>
         <div className={styles.border}></div>
       </div>
+      <div className={styles.galleryScrolling}>
+        <button className={styles.mobileScroll} onClick={() => handleScroll("left")} disabled={currentIndex === 0} style={{ display: isMobileView ? 'block' : 'none' }}>
+          <Image src="/img/arrow-left.png" alt="arrow-left" width={40} height={60} className={styles.arrow1} />
+        </button>
       <ul className={styles.picturesList}>
         {visiblePictures.map((picture) => (
           <li key={picture.id}>
-            <Image
-              src={picture.image}
-              alt="photo gallery"
-              width={490}
-              height={380}
-              className={styles.galleryImage}
-            />
+            <Image src={picture.image} alt="photo gallery" width={490} height={380} className={styles.galleryImage} />
           </li>
         ))}
       </ul>
-      <div className={styles.scrollers}>
-        <button onClick={() => handleScroll("left")}>
+        <button className={styles.mobileScroll} onClick={() => handleScroll("right")} disabled={currentIndex >= pictures.length - (isMobileView ? 1 : 3)} style={{ display: isMobileView ? 'block' : 'none' }}>
+          <Image src="/img/arrow-right.png" alt="arrow-right" width={40} height={60} className={styles.arrow1} />
+        </button>
+      <div className={styles.scrollers} style={{ display: isMobileView ? 'none' : 'flex' }}>
+      <button onClick={() => handleScroll("left")}>
           <Image
             src="/img/arrow-left.png"
             alt="arrow-left"
@@ -93,6 +80,7 @@ const Gallery = () => {
             className={styles.arrow1}
           />
         </button>
+      </div>
       </div>
     </section>
   );
--- src/components/Printers.js
+++ src/components/Printers.js
@@ -1,8 +1,53 @@
+"use client";
 import Image from "next/image";
 import styles from "../app/page.module.css";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 const Printers = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  const printers = [
+    {
+      name: "Ruby (R Lite / R Pro)",
+      image: "/img/ruby.png",
+      link: "/printers/ruby",
+      className: styles.printer1
+    },
+    {
+      name: "Sapphire (S Lite / S Pro)",
+      image: "/img/sapphire.png",
+      link: "/printers/sapphire",
+      className: styles.printer2
+    },
+    {
+      name: "Heliodorus (H Lite / H Pro)",
+      image: "/img/heliodorus.png",
+      link: "/printers/heliodorus",
+      className: styles.printer3
+    }
+  ];
+
+  const handleScroll = (direction) => {
+    if (direction === "left" && currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    } else if (direction === "right" && currentIndex < printers.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+    const updateView = () => {
+      setIsMobileView(window.innerWidth <= 1000);
+    };
+
+    updateView();
+
+    window.addEventListener("resize", updateView);
+    return () => window.removeEventListener("resize", updateView);
+  }, []);
+
   return (
     <section className={styles.sectionPrinters} id="printers">
       <div className={styles.printersText}>
@@ -14,87 +59,82 @@ const Printers = () => {
             свойства и максимальные габариты создаваемого изделия
           </p>
           <ul className={styles.industriesList}>
-            <li>
-              <p>ювелирное дело</p>
-            </li>
-            <li>
-              <p>стоматология</p>
-            </li>
-            <li>
-              <p>машиностроение</p>
-            </li>
-            <li>
-              <p>медицина</p>
-            </li>
-            <li>
-              <p>автомобилестроение</p>
-            </li>
-            <li>
-              <p>архитектура</p>
-            </li>
-            <li>
-              <p>прототипирование</p>
-            </li>
-            <li>
-              <p>текстильная промышленность</p>
-            </li>
-            <li>
-              <p>обувная промышленность</p>
-            </li>
+            <li><p>ювелирное дело</p></li>
+            <li><p>стоматология</p></li>
+            <li><p>машиностроение</p></li>
+            <li><p>медицина</p></li>
+            <li><p>автомобилестроение</p></li>
+            <li><p>архитектура</p></li>
+            <li><p>прототипирование</p></li>
+            <li><p>текстильная промышленность</p></li>
+            <li><p>обувная промышленность</p></li>
           </ul>
         </div>
       </div>
+
       <div className={styles.printersPrinters}>
+        {isMobileView && (
+          <button onClick={() => handleScroll("left")} disabled={currentIndex === 0} className={styles.arrowsMobile}>
+            <Image
+              src="/img/arrow-left.png"
+              alt="arrow-left"
+              width={40}
+              height={60}
+              className={styles.arrow1Printers}
+            />
+          </button>
+        )}
+        
         <ul className={styles.printersList}>
-          <li className={styles.printersListEl}>
-            <Image
-              className={styles.printer1}
-              alt="ruby-printer"
-              src={"/img/ruby.png"}
-              width={10000}
-              height={15000}
-            />
-            <h3>Ruby (R Lite / R Pro)</h3>
-            <div>
-              <Link href="/printers/ruby" legacyBehavior>
-                <a className={styles.moreLink}>Подробнее</a>
-              </Link>
-              <button>Заказать</button>
-            </div>
-          </li>
-          <li className={styles.printersListEl}>
-            <Image
-              className={styles.printer2}
-              alt="ruby-printer"
-              src={"/img/sapphire.png"}
-              width={10000}
-              height={15000}
-            />
-            <h3>Sapphire (S Lite / S Pro)</h3>
-            <div>
-              <Link href="/printers/sapphire" legacyBehavior>
-                <a className={styles.moreLink}>Подробнее</a>
-              </Link>
-              <button>Заказать</button>
-            </div>
-          </li>
-          <li className={styles.printersListEl}>
-            <Image
-              className={styles.printer3}
-              alt="ruby-printer"
-              src={"/img/heliodorus.png"}
-              width={10000}
-              height={15000}
-            />
-            <h3>Heliodorus (H Lite / H Pro)</h3>
-            <div>
-              <Link href="/printers/heliodorus" legacyBehavior>
-                <a className={styles.moreLink}>Подробнее</a>
-              </Link>
-              <button>Заказать</button>
-            </div>
-          </li>
+          {isMobileView ? (
+            <li className={styles.printersListEl}>
+              <Image
+                className={printers[currentIndex].className}
+                alt={`${printers[currentIndex].name}-printer`}
+                src={printers[currentIndex].image}
+                width={10000}
+                height={15000}
+              />
+              <h3>{printers[currentIndex].name}</h3>
+              <div>
+                <Link href={printers[currentIndex].link} legacyBehavior>
+                  <a className={styles.moreLink}>Подробнее</a>
+                </Link>
+                <button>Заказать</button>
+              </div>
+            </li>
+          ) : (
+            printers.map((printer, index) => (
+              <li key={index} className={styles.printersListEl}>
+                <Image
+                  className={printer.className}
+                  alt={`${printer.name}-printer`}
+                  src={printer.image}
+                  width={10000}
+                  height={15000}
+                />
+                <h3>{printer.name}</h3>
+                <div>
+                  <Link href={printer.link} legacyBehavior>
+                    <a className={styles.moreLink}>Подробнее</a>
+                  </Link>
+                  <button>Заказать</button>
+                </div>
+              </li>
+            ))
+          )}
         </ul>
+        {isMobileView && (
+          <button onClick={() => handleScroll("right")} disabled={currentIndex === printers.length - 1} className={styles.arrowsMobile}>
+            <Image
+              src="/img/arrow-right.png"
+              alt="arrow-right"
+              width={40}
+              height={60}
+              className={styles.arrow1Printers}
+            />
+          </button>
+        )}
       </div>
     </section>
   );
